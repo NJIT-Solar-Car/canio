93faf55d4d1250b8b7f9a2e248ce35bb51cf8560
Arnav Revankar <bumpyturtle127@gmail.com>
2026-07-14 01:16:19 -0400

    another change

--- temp-software/main.c
+++ temp-software/main.c
@@ -9,12 +9,14 @@
 #define INDICATOR_RIGHT	PORTD3
 #define HAZARD_LEFT		PORTD4
 #define HAZARD_RIGHT	PORTD5
+#define HORN			PORTD6
 
 #define LEFT_INPUT		PORTC2
 #define RIGHT_INPUT		PORTC3
 #define HAZARD_INPUT	PORTC4
+#define HORN_INPUT		PORTC5
 
-uint8_t left_state, right_state, hazard_state;
+uint8_t left_state, right_state, hazard_state, horn_state;
 
 uint8_t getInput();
 
@@ -66,6 +68,18 @@ int main(void) {
 					PORTD &= ~((1 << HAZARD_LEFT) | (1 << HAZARD_RIGHT));
 					hazard_state = 0;
 				}
+			case 4:
+				// Horn
+				if (!horn_state) {
+					PORTD |= (1 << HORN);
+					horn_state = 1;		
+				}
+			default:
+				PORTD = 0x0;
+				horn_state = 0;
+				hazard_state = 0;
+				left_state = 0;
+				right_state = 0;
 		}
 
         _delay_ms(250);
@@ -76,12 +90,14 @@ int main(void) {
 
 uint8_t getInput() {
 	// Naive code assumes one-hot situation type thing
-	if (PIND & (1 << LEFT_INPUT)) {
+	if (PINC & (1 << LEFT_INPUT)) {
 		return 1;
-	} else if (PIND & (1 << RIGHT_INPUT)) {
+	} else if (PINC & (1 << RIGHT_INPUT)) {
 		return 2;
-	} else if (PIND & (1 << HAZARD_INPUT)) {
+	} else if (PINC & (1 << HAZARD_INPUT)) {
 		return 3;
+	} else if (PINC & (1 << HORN)) {
+		return 4;
 	}
 	return 0;
 }
