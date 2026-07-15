e5ba588ec0dd2e4c2a5910813ea261426e13c3ec
Arnav Revankar <bumpyturtle127@gmail.com>
2026-07-14 20:49:49 -0400

    another change

--- temp-software/main.c
+++ temp-software/main.c
@@ -7,107 +7,110 @@
 
 #define INDICATOR_LEFT	PORTD2
 #define INDICATOR_RIGHT	PORTD3
-#define HAZARD_LEFT		PORTD4
-#define HAZARD_RIGHT	PORTD5
-#define HORN			PORTD6
+#define HORN_OUT		PORTD4
+#define HAZARD_LEFT		PORTD5
+#define HAZARD_RIGHT	PORTD6
+#define HEADLIGHT_LEFT	PORTD7
+#define HEADLIGHT_RIGHT	PORTB0
 
-#define LEFT_INPUT		PORTC2
-#define RIGHT_INPUT		PORTC3
-#define HAZARD_INPUT	PORTC4
-#define HORN_INPUT		PORTC5
+#define LEFT_INPUT		PORTC0
+#define RIGHT_INPUT		PORTC1
+#define HORN_INPUT		PORTC2
+#define HAZARD_INPUT	PORTC3
+#define HEADLIGHT_INPUT	PORTC4
 
-uint8_t left_state, right_state, hazard_state, horn_state;
+uint8_t left_state = 0;
+uint8_t right_state = 0;
+uint8_t hazard_state = 0;
+uint8_t horn_state = 0;
+uint8_t headlight_state = 0;
 
-uint8_t getInput();
+void configure();
 
 int main(void) {
-    // Configure lights as outputs
+	configure();
+
+	while (1) {
+		if (PINC & (1 << LEFT_INPUT)) {
+			if (!left_state) {
+				PORTD |= (1 << INDICATOR_LEFT);
+				left_state = 1;
+			} else {
+				PORTD &= ~(1 << INDICATOR_LEFT);
+				left_state = 0;
+			}
+		}
+
+		if (PINC & (1 << RIGHT_INPUT)) {
+			if (!right_state) {
+				PORTD |= (1 << INDICATOR_RIGHT);
+				right_state = 1;
+			} else {
+				PORTD &= ~(1 << INDICATOR_RIGHT);
+				right_state = 0;
+			}
+		}
+
+		if (PINC & (1 << HAZARD_INPUT)) {
+			if (!hazard_state) {
+				PORTD |= (1 << HAZARD_LEFT) | (1 << HAZARD_RIGHT);
+				hazard_state = 1;
+			} else {
+				PORTD &= ~((1 << HAZARD_LEFT) | (1 << HAZARD_RIGHT));
+				hazard_state = 0;
+			}
+		}
+
+		if (PINC & (1 << HORN_INPUT)) {
+			if (!horn_state) {
+				PORTD |= (1 << HORN_OUT);
+				horn_state = 1;		
+			}
+		}
+
+		if (PINC & (1 << HEADLIGHT_INPUT)) {
+			if (!headlight_state) {
+				PORTD |= (1 << HEADLIGHT_LEFT);
+				PORTB |= (1 << HEADLIGHT_RIGHT);
+				headlight_state = 1;
+			}
+		}
+
+		// Only wait if lights are on
+        if (PORTD != 0x0 || PORTB != 0x0) {
+			_delay_ms(250);
+		}
+	}
+
+    return 0; // never reached
+}
+
+void configure() {
+    // Configure outputs
     DDRD |= (1 << INDICATOR_LEFT) |
 			(1 << INDICATOR_RIGHT) |
+			(1 << HORN_OUT) |
 			(1 << HAZARD_LEFT) |
-			(1 << HAZARD_RIGHT);
+			(1 << HAZARD_RIGHT) |
+			(1 << HEADLIGHT_LEFT);
+
+	DDRB |= (1 << HEADLIGHT_RIGHT);
 
 	
 	// Configure GPIOs as inputs
 	DDRC &= ~((1 << LEFT_INPUT) |
 			(1 << RIGHT_INPUT) |
+			(1 << HORN_INPUT) |
 			(1 << HAZARD_INPUT) |
-			(1 << HORN_INPUT));
+			(1 << HEADLIGHT_INPUT));
 
-    // turn all leds off
+    // turn all outputs off
     PORTD &= ~((1 << INDICATOR_LEFT) |
 				(1 << INDICATOR_RIGHT) |
+				(1 << HORN_OUT) |
 				(1 << HAZARD_LEFT) |
 				(1 << HAZARD_RIGHT) |
-				(1 << HORN));
+				(1 << HEADLIGHT_LEFT));
 
-    while (1) {
-		switch (getInput()) {
-			case 1:
-				// Left indicator
-				if (!left_state) {
-					PORTD |= (1 << INDICATOR_LEFT);
-					left_state = 1;
-				} else {
-					PORTD &= ~(1 << INDICATOR_LEFT);
-					left_state = 0;
-				}
-				break;
-			case 2:
-				// Right indicator
-				if (!right_state) {
-					PORTD |= (1 << INDICATOR_RIGHT);
-					right_state = 1;
-				} else {
-					PORTD &= ~(1 << INDICATOR_RIGHT);
-					right_state = 0;
-				}
-				break;
-			case 3:
-				// Hazards
-				if (!hazard_state) {
-					PORTD |= (1 << HAZARD_LEFT) | (1 << HAZARD_RIGHT);
-					hazard_state = 1;
-				} else {
-					PORTD &= ~((1 << HAZARD_LEFT) | (1 << HAZARD_RIGHT));
-					hazard_state = 0;
-				}
-				break;
-			case 4:
-				// Horn
-				if (!horn_state) {
-					PORTD |= (1 << HORN);
-					horn_state = 1;		
-				}
-				break;
-			default:
-				PORTD = 0x0;
-				horn_state = 0;
-				hazard_state = 0;
-				left_state = 0;
-				right_state = 0;
-				break;
-		}
-
-		// Only wait if lights are on
-        if (PORTD != 0x0) {
-			_delay_ms(250);
-		}
-    }
-
-    return 0; // Standard flow control requirement, never reached
-}
-
-uint8_t getInput() {
-	// Naive code assumes one-hot situation type thing
-	if (PINC & (1 << LEFT_INPUT)) {
-		return 1;
-	} else if (PINC & (1 << RIGHT_INPUT)) {
-		return 2;
-	} else if (PINC & (1 << HAZARD_INPUT)) {
-		return 3;
-	} else if (PINC & (1 << HORN_INPUT)) {
-		return 4;
-	}
-	return 0;
+	PORTB &= ~(1 << HEADLIGHT_RIGHT);
 }
